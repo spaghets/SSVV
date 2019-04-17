39b1df69ffbb685a747234d5a8ce5e876e83259c
Spaghets <tudorcatana@protonmail.com>
2019-04-17 13:37:18 +0300

    initial

---
+++ src/test/java/test/java/lab4tests.java
@@ -0,0 +1,92 @@
+package test.java;
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.*;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+import static org.junit.Assert.assertEquals;
+
+public class lab4tests {
+    @Test
+    public void testAddAssignment () {
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.deleteTema("12");
+        assertEquals (service.saveTema("12", "test", 10, 3) ,1);
+        assertEquals (service.saveTema("12", "test", 10, 3) ,0);
+
+
+
+
+    }
+
+    public void testAddStudent () {
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.deleteTema("99");
+        assertEquals (service.saveStudent("99", "Vasile", 321) ,1);
+
+
+
+    }
+
+    @Test
+    public void testAddGrade() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        assertEquals(service.saveNota("1","1",10,12, "nice"),0);
+
+
+    }
+
+    @Test
+    public void integartionTest() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note3.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.deleteTema("19");
+        service.deleteStudent("101");
+        service.saveTema("19", "test", 10, 3);
+        service.saveStudent("101", "Vasi", 322);
+
+        assertEquals(service.saveNota("101","19",10,10,"super super"),1);
+    }
+}
---
+++ src/test/java/test/java/tests.java
@@ -0,0 +1,76 @@
+package test.java;
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import org.junit.*;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+import static org.junit.Assert.assertEquals;
+
+public class tests {
+    @Test
+     public void testAddAssignment () {
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        assertEquals (service.saveTema("12", "test", 10, 3) ,1);
+        assertEquals (service.saveTema("12", "test", 10, 3) ,0);
+
+
+
+    }
+    @Test
+    public void testAddInvalidAssignment () {
+
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        assertEquals (service.saveTema("12", "test", 15, 3) ,0);
+
+
+
+
+    }
+
+    @Test
+    public void testAddGrade() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+
+        assertEquals(service.saveNota("1","1",10,12, "nice"),0);
+
+
+    }
+
+
+
+}
