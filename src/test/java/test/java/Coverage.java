cbbf7f4bae1ac1f73dec371686d33fd682c74825
Spaghets <tudorcatana@protonmail.com>
2019-05-07 20:26:05 +0300

    lab3 home

--- src/test/java/test/java/Coverage.java
+++ src/test/java/test/java/Coverage.java
@@ -16,10 +16,10 @@ import validation.Validator;
 public class Coverage {
 
     @Test
-    public void addAssignmentC1()
+    public void addAssignmentValid()
     {
-        int count = 0;
-        int countafter = 0;
+        int assignmentsBefore = 0;
+        int assignmentsAfter = 0;
 
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
@@ -32,19 +32,21 @@ public class Coverage {
         Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         for (Tema assignment : service.findAllTeme())
         {
-            count += 1;
+            assignmentsBefore += 1;
         }
-        service.saveTema(Integer.toString(count + 100), "Test descr", 4, 2);
+        service.saveTema(Integer.toString(assignmentsBefore + 100), "Description", 4, 2);
         for (Tema assignment : service.findAllTeme())
         {
-            countafter += 1;
+
+            assignmentsAfter += 1;
         }
-        assert(count + 1 == countafter);
+        assert(assignmentsBefore + 1 == assignmentsAfter);
     }
 
     @Test
-    public void addAssignmentC2()
+    public void addAssignmentInvalid()
     {
+        //input data is invalid (the id is incorrect) the test should fail
         int count = 0;
         int countafter = 0;
 
@@ -70,8 +72,9 @@ public class Coverage {
     }
 
     @Test
-    public void addAssignmentC3()
+    public void addAssignmentInvalid2()
     {
+        //input data is invalid (deadline date is before the starting date) so the test should fail
         int count = 0;
         int countafter = 0;
 
